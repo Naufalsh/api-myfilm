b0e3c9bf58ec0b23d89387c13406588d2b81442c
Naufal Maulana Artocarpus Savero <naufalmaulanaas@gmail.com>
2025-06-25 15:53:45 +0700

    push

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GalleryController;
+use App\Models\Gallery;
 
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //     return $request->user();
@@ -15,7 +17,22 @@ Route::delete('/reviews/delete/{id}', [ReviewController::class, 'destroy']);
 
 Route::get('/reviews', [ReviewController::class, 'index']);
 
-//
+//route gallery
+Route::get('items/all', [GalleryController::class, 'allItems']);
+Route::get('items', [GalleryController::class, 'index']);
+Route::post('items', [GalleryController::class, 'store']);
+Route::get('items/{item}', [GalleryController::class, 'show']);
+Route::post('items/{item}', [GalleryController::class, 'update']);
+Route::patch('items/{item}', [GalleryController::class, 'update']);
+Route::delete('items/{item}', [GalleryController::class, 'destroy']);
+Route::get('images/{id}', function ($id) {
+    $ket = Gallery::where('id', $id)->firstOrFail();
+    $path = storage_path('app/public/' . $ket->gambar);
+    if (!file_exists($path)) {
+        return response()->json(['message' => 'Image not found'], 404);
+    }
+    return response()->file($path);
+});
 
 
 require __DIR__ . '/auth.php';
